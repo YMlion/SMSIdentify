6411229069cc2a0c59467a683c22b7303e764e54
YMlion <lm_yao@163.com>
2017-07-17 21:08:16 +0800

    reg match

--- app/src/main/java/com/ymlion/smsidentify/SmsReceiver.java
+++ app/src/main/java/com/ymlion/smsidentify/SmsReceiver.java
@@ -1,11 +1,13 @@
 package com.ymlion.smsidentify;
 
 import android.content.BroadcastReceiver;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
-import android.util.Log;
+import android.widget.Toast;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -32,14 +34,27 @@ public class SmsReceiver extends BroadcastReceiver {
                 msg = SmsMessage.createFromPdu((byte[]) object);
                 msgBody.append(msg.getDisplayMessageBody());
             }
-            String regEx = "#\\d+#Y";
-            Pattern pattern = Pattern.compile(regEx);
-            Matcher matcher = pattern.matcher(msgBody.toString());
-            if (matcher.find()) {
-                Log.e(TAG, "onReceive: true " + msgBody);
-            } else {
-                Log.e(TAG, "onReceive: false " + msgBody);
+            String code = findCode(msgBody.toString());
+
+            if (code != null) {
+                ClipboardManager cm = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("SMS", code);
+                cm.setPrimaryClip(clip);
+                Toast.makeText(context, "验证码：" + code + " 已经复制到剪贴板", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    public static String findCode(String msg) {
+        if (msg.length() > 3 && msg.contains("验证码")) {
+            String regEx="(\\d{6})|(\\d{4})";
+            Pattern p = Pattern.compile(regEx);
+            Matcher m = p.matcher(msg);
+            if (m.find()) {
+                return m.group();
+            }
+        }
+
+        return null;
     }
 }
